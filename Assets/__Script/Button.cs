b7c0215a5ef99cf6c8c2f154dd44c0977a28b0d9
bcook03 <bnc121203@outlook.com>
2025-04-13 12:05:44 -0500

    WebGL Build and fixed Start Button

--- Assets/__Script/Button.cs
+++ Assets/__Script/Button.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     public void GameStart() {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 
     public void ReturnToMenu(){
